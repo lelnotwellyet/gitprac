2b981fbbc9a7b38f08fc250b12f951515c1963a6
lelnotwellyet <lalmohammadpatel11@gmail.com>
2025-10-15 09:38:09 +0530

    added subtraction

--- gitprac/src/App.js
+++ gitprac/src/App.js
@@ -5,11 +5,18 @@ function App() {
   const [num1, setNum1] = useState(0);
   const [num2, setNum2] = useState(0);
   const [result, setResult] = useState(0);
+  const [subresult, setSubResult] = useState(0);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const sum = parseInt(num1) + parseInt(num2);
     setResult(sum);
+  }
+
+  const handleSubtract = (e) => {
+    e.preventDefault();
+    const sub = parseInt(num1) - parseInt(num2);
+    setSubResult(sub);
   }
 
   return (
@@ -22,7 +29,9 @@ function App() {
           onChange={(e)=>setNum2(e.target.value)}>
           </input>
           <button type="submit">Add</button>
-          <h2>Result: {result}</h2>
+          <button type="button" onClick={handleSubtract}>Sub</button>
+          <h2>Additon Result: {result}</h2>
+          <h2>Subtract Result: {subresult}</h2>
       </form>
     </div>
   );
