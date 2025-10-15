6f87573fcc8b598c54b0b8e789f779e2551b1e61
lelnotwellyet <lalmohammadpatel11@gmail.com>
2025-10-15 09:16:38 +0530

    first commit

---
+++ gitprac/src/App.js
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+
+function App() {
+  //Addition of 2 numbers
+  const [num1, setNum1] = useState(0);
+  const [num2, setNum2] = useState(0);
+  const [result, setResult] = useState(0);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const sum = parseInt(num1) + parseInt(num2);
+    setResult(sum);
+  }
+
+  return (
+    <div>
+      <form onSubmit ={handleSubmit}>
+          <input type="number" placeholder="Enter first number" value={num1}
+          onChange={(e)=>setNum1(e.target.value)}
+          ></input>
+          <input type="number" placeholder="Enter second number" value={num2}
+          onChange={(e)=>setNum2(e.target.value)}>
+          </input>
+          <button type="submit">Add</button>
+          <h2>Result: {result}</h2>
+      </form>
+    </div>
+  );
+}
+
+export default App;
